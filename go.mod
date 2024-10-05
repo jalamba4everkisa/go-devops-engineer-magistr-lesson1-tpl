15913de518e3ccf7dd47d8b438f7ee91e00112ed
jalamba4everkisa <jalamba4everkisa@gmail.com>
2024-10-05 22:58:26 +0300

    testing

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/jalamba4everkisa/go-devops-engineer-magistr-lesson1-tpl
+
+go 1.23.1
--- main.go
+++ main.go
@@ -1,3 +1,26 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
+
+func main() {
+	response, err := http.Get("http://srv.msk01.gigacorp.local")
+	if err != nil {
+		fmt.Println(err)
+	}
+	if response.StatusCode == 200 {
+		fmt.Printf("Status Code: %d\r\n", response.StatusCode)
+		body, err := io.ReadAll(response.Body)
+		defer response.Body.Close()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Println(string(body))
+	} else {
+		fmt.Println("Unable to fetch server statistic")
+	}
+}
