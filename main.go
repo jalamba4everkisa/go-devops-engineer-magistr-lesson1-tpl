2de8068b1921df25e449b818111b36065eefa5d0
jalamba4everkisa <jalamba4everkisa@gmail.com>
2024-10-06 20:48:04 +0300

    added infinite for loop

--- main.go
+++ main.go
@@ -7,47 +7,52 @@ import (
 	"net/http"
 	"strconv"
 	"strings"
+	"time"
 
 	"github.com/dariubs/percent"
 )
 
 func main() {
-	var checkbox = 0
-	response, err := http.Get("http://srv.msk01.gigacorp.local")
-	if err != nil {
-		fmt.Println(err)
-		checkbox += 1
-	}
-	if response.StatusCode != 200 {
-		checkbox += 1
-	}
-	body, err := io.ReadAll(response.Body)
-	//defer
-	response.Body.Close()
-	if err != nil {
-		checkbox += 1
-		fmt.Println(err)
-		return
-	}
-	bodyStrings := strings.Split(string(body), ",")
-	bodyInt := []int{}
-	for _, i := range bodyStrings {
-		j, err := strconv.Atoi(strings.Trim(i, " "))
+	for {
+		var checkbox = 0
+		response, err := http.Get("http://srv.msk01.gigacorp.local")
 		if err != nil {
-			bodyInt = append(bodyInt, 1)
+			fmt.Println(err)
 			checkbox += 1
-		} else {
-			bodyInt = append(bodyInt, j)
 		}
+		if response.StatusCode != 200 {
+			checkbox += 1
+		}
+		body, err := io.ReadAll(response.Body)
+		//defer
+		response.Body.Close()
+		if err != nil {
+			checkbox += 1
+			fmt.Println(err)
+			return
+		}
+		bodyStrings := strings.Split(string(body), ",")
+		bodyInt := []int{}
+		for _, i := range bodyStrings {
+			j, err := strconv.Atoi(strings.Trim(i, " "))
+			if err != nil {
+				bodyInt = append(bodyInt, 1)
+				checkbox += 1
+			} else {
+				bodyInt = append(bodyInt, j)
+			}
+		}
+		if checkbox > 2 {
+			fmt.Println("Unable to fetch server statistic")
+		} else {
+			check(memoryUsage(bodyInt[2], bodyInt[1]))
+			check(diskUsage(bodyInt[4], bodyInt[3]))
+			check(loadAverage(bodyInt[0]))
+			check(networkUsage(bodyInt[6], bodyInt[5]))
+		}
+		time.Sleep(15 * time.Second)
 	}
-	if checkbox > 2 {
-		fmt.Println("Unable to fetch server statistic")
-	} else {
-		check(memoryUsage(bodyInt[2], bodyInt[1]))
-		check(diskUsage(bodyInt[4], bodyInt[3]))
-		check(loadAverage(bodyInt[0]))
-		check(networkUsage(bodyInt[6], bodyInt[5]))
-	}
+
 }
 
 func loadAverage(la int) string {
